2382f1d8114c5777fac8b5fa26d1317a1fa45ed5
Jonathanjohansson24 <jonathan.nordin24@gmail.com>
2023-09-25 13:46:49 +0200

    added input from users

--- Program.cs
+++ Program.cs
@@ -7,6 +7,15 @@
             Console.WriteLine("Hello, World!");
             Console.WriteLine("Hej");
             Console.WriteLine("hej aska och john");
+
+            Console.Write("Mata in första talet: ");
+            int firstNumber = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Mata in andra talet: ");
+            int secondNumber = Convert.ToInt32(Console.ReadLine()); 
         }
+
+
+
+       
     }
 }
